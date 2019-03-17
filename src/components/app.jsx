cbc409fcb2b2489effe9e99646b4d5f4c11d1d4b
Miguel Ferrer <miguef7@gmail.com>
2019-03-16 20:49:42 -0500

    refactoring

---
+++ src/components/app.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
+
+import FormSearch from './formSearch';
+import Gif from './gif';
+import GifList from './gifList';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gifs: [],
+      selectedGif: "KVVNGUK06y18Ez6wo9",
+    };
+  }
+
+  search = (query) => {
+    giphy('75YvgQ2vJAHWe81iKLLk2ZYAEHTRdaCf').search({
+      q: query,
+      limit: 10
+    }, (error, result) => {
+      this.setState({ gifs: result.data });
+    });
+  }
+
+  changeSelectedGif = (id) => {
+    this.setState({
+      selectedGif: id
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <FormSearch searchFunction={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGif} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} selectedGif={this.changeSelectedGif} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,53 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import giphy from 'giphy-api';
 
-import FormSearch from './components/formSearch';
-import Gif from './components/gif';
-import GifList from './components/gifList';
+import App from './components/app';
 
 import '../assets/stylesheets/application.scss';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      gifs: [],
-      selectedGif: "KVVNGUK06y18Ez6wo9",
-    };
-  }
-
-  search = (query) => {
-    giphy('75YvgQ2vJAHWe81iKLLk2ZYAEHTRdaCf').search({
-      q: query,
-      limit: 10
-    }, (error, result) => {
-      this.setState({ gifs: result.data });
-    });
-  }
-
-  changeSelectedGif = (id) => {
-    this.setState({
-      selectedGif: id
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="left-scene">
-          <FormSearch searchFunction={this.search} />
-          <div className="selected-gif">
-            <Gif id={this.state.selectedGif} />
-          </div>
-        </div>
-        <div className="right-scene">
-          <GifList gifs={this.state.gifs} selectedGif={this.changeSelectedGif} />
-        </div>
-      </div>
-    );
-  }
-}
 
 const root = document.getElementById('root');
 if (root) {
